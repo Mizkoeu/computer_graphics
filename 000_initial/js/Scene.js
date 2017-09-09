c6861f52fac989414d1d17cf7bd3884b0527174b
Mike Zou <zoutianh@grinnell.edu>
2017-09-09 13:06:49 +0200

    initial GPU pipeline

---
+++ 000_initial/js/Scene.js
@@ -0,0 +1,26 @@
+"use strict";
+let Scene = function(gl) {
+  this.vsIdle = new Shader(gl, gl.VERTEX_SHADER, "idle_vs.essl");
+  this.fsSolid = new Shader(gl, gl.FRAGMENT_SHADER, "solid_fs.essl");
+  this.solidProgram = new Program(gl, this.vsIdle, this.fsSolid);
+  this.triangleGeometry = new TriangleGeometry(gl);
+
+  this.timeAtLastFrame = new Date().getTime();
+
+};
+
+Scene.prototype.update = function(gl, keysPressed) {
+  //jshint bitwise:false
+  //jshint unused:false
+  let timeAtThisFrame = new Date().getTime();
+  let dt = (timeAtThisFrame - this.timeAtLastFrame) / 1000.0;
+  this.timeAtLastFrame = timeAtThisFrame;
+
+  // clear the screen
+  gl.clearColor(0, 0, 0.8, 1.0);
+  gl.clearDepth(1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  this.solidProgram.commit();
+  this.triangleGeometry.draw();
+};
